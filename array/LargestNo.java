b075f7bc389eefc7e44eaf4a67636903f339f1dd
sairaskar <sairaskar26@gmail.com>
2025-10-15 23:36:12 +0530

    Added LargestNo.java file

--- array/LargestNo.java
+++ array/LargestNo.java
@@ -1,1 +1,12 @@
-This is a new file
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 2, 8, 6};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest element: " + max);
+    }
+}
